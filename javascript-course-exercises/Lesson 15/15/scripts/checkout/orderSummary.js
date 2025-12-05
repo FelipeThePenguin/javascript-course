f18995e5359b27cc29670cf3c3e6bad808296ed5
FelipeThePenguin <ChrisRockHatesEveryone@gmail.com>
2025-12-05 11:16:19 +0800

    Exercise 15h

--- javascript-course-exercises/Lesson 15/15/scripts/checkout/orderSummary.js
+++ javascript-course-exercises/Lesson 15/15/scripts/checkout/orderSummary.js
@@ -119,9 +119,8 @@ export function renderOrderSummary() {
       removeFromCart(productId);
       
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
-      
-      container.remove();
-      
+    
+      renderOrderSummary();
       renderPaymentSummary();
     });
   });
