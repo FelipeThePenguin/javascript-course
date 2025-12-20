9b7ff7898e90370ea403d136c77a3cea38a46f82
FelipeThePenguin <ChrisRockHatesEveryone@gmail.com>
2025-12-20 20:45:18 +0800

    Exercise 16j

--- javascript-course-exercises/Lesson 16/tests/checkout/orderSummaryTest.js
+++ javascript-course-exercises/Lesson 16/tests/checkout/orderSummaryTest.js
@@ -80,4 +80,16 @@ describe('test suite: renderOrderSummary', () => {
     expect(cart[0].productId).toEqual(productId2);
     
   });
+  
+  it('updates the delivery option', () => {
+    document.querySelector(`.js-delivery-option-${productId1}-3`).click();
+
+    expect(document.querySelector(`.js-delivery-option-input-${productId1}-3`).checked).toEqual(true);
+    expect(cart.length).toEqual(2);
+    expect(cart[0].productId).toEqual(productId1);
+    expect(cart[0].deliveryOptionId).toEqual('3');
+    expect(document.querySelector('.js-payment-summary-money-shipping-price').innerText).toContain('$14.98');
+    expect(document.querySelector('.js-payment-summary-money-total-price').innerText).toContain('$63.50');
+  });
+  
 });
