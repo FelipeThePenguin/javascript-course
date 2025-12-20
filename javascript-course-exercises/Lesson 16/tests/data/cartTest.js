dfd90f06b8ddb759bdfbd5f9588f74c75eb0026d
FelipeThePenguin <ChrisRockHatesEveryone@gmail.com>
2025-12-20 21:16:22 +0800

    Exercise 16k

--- javascript-course-exercises/Lesson 16/tests/data/cartTest.js
+++ javascript-course-exercises/Lesson 16/tests/data/cartTest.js
@@ -1,4 +1,6 @@
-import {addToCart, removeFromCart, cart, loadFromStorage} from '../../data/cart.js';
+import {addToCart, removeFromCart, updateDeliveryOption, cart, loadFromStorage} from '../../data/cart.js';
+
+const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
 
 describe('test suite: addToCart', () => {
   
@@ -97,6 +99,33 @@ describe('test suite: removeFromCart', () => {
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toEqual(1);
+  });
+  
+});
+
+describe('test suite: updateDeliveryOption', () => {
+  
+  it('updates the delivery option of a product in the cart', () => {
+    spyOn(localStorage, 'setItem');
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: productId1,
+        quantity: 1,
+        deliveryOptionId: '1'
+    }])
+    });
+    
+    loadFromStorage();
+    updateDeliveryOption(productId1, '3');
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual(productId1);
+    expect(cart[0].deliveryOptionId).toEqual('3');
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+  productId: productId1,
+  quantity: 1,
+  deliveryOptionId: '3'
+}]));
   });
   
 });
