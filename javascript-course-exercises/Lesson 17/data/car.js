45f54294785e88af409b0de25e57bdce21b3fe21
FelipeThePenguin <ChrisRockHatesEveryone@gmail.com>
2025-12-22 21:04:14 +0800

    Exercise 17b

--- javascript-course-exercises/Lesson 17/data/car.js
+++ javascript-course-exercises/Lesson 17/data/car.js
@@ -6,16 +6,26 @@ class Car {
     this.brand = carDetails.brand;
     this.model = carDetails.model;
   }
+  
+  displayInfo() {
+    console.log(`${this.brand} - ${this.model}`);
+  }
+  
 }
 
 const car1 = new Car({
   brand: 'Toyota',
   model: 'Corolla'
 });
-const car2= new Car({
+const car2 = new Car({
   brand: 'Tesla',
   model: 'Model 3'
 });
 
+/*
 console.log(car1);
 console.log(car2);
+*/
+
+car1.displayInfo();
+car2.displayInfo();
